00078146d11a4a403a4fe544965457a249101585
edmar012 <155861109+edmar012@users.noreply.github.com>
2024-01-11 14:28:36 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(5000, ()=> {
+    console.log("Server is active"
+    )
+})
+const { Configuration, OpenAIApi} = require("openai")
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: 'your-api-key'
+}))
+
+app.post('/chat', async (req, res)=>{
+    try {
+        const resp = await openai.creatChatCompilation({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: "user", content: req.body.question}
+            ]
+        })
+
+        res.status(200).json({message: resp.data.choices[0].message.content})
+    } catch(e) {
+        res.status(400).json({message: e.message})
+    }
+})
